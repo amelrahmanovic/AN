77815c267bf19a8638b20034bb6e0e6c1ee92f8a
Amel <amelrahmanovic@hotmail.com>
2019-12-01 20:27:11 +0100

    Booking-add.component add one object to DOM and need more...

--- src/app/Booking.ts
+++ src/app/Booking.ts
@@ -1,7 +1,7 @@
 export default class BookingModel {
   id: number;
-  object_name: string;
   capacity_provider_id: number;
+  object_name: string;
   date_from: Date;
   date_to: Date;
   is_active: boolean;
--- src/app/booking-add/booking-add.component.ts
+++ src/app/booking-add/booking-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Booking from '../Booking'
 
 @Component({
   selector: 'app-booking-add',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking-add.component.css']
 })
 export class BookingAddComponent implements OnInit {
-
+  bookings: Array<Booking> = new Array<Booking>();
+  book: Booking;
   constructor() { }
+
+  add(id, capacity_provider_id, Object, From, To, is_active, amount, currency, comment) {
+
+    let x = new  Booking();
+    x.id = 1;
+    x.capacity_provider_id = 1;
+    x.object_name = Object;
+    x.date_from = From;
+    x.date_to = To;
+    x.is_active = is_active;
+    x.amount = amount;
+    x.currency = currency;
+    x.comment = comment;
+
+    this.bookings.push(x);
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let index = 0; index < this.bookings.length; index++) {
+      console.log(this.bookings[index].comment);
+    }
+  }
 
   ngOnInit() {
   }
-
 }
