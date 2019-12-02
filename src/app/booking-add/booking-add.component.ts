47303cd989a260ca73a41777c7fe4acc004d33d2
Amel <amelrahmanovic@hotmail.com>
2019-12-02 20:42:46 +0100

    Bug: replacing complicated code with simple code :)

--- src/app/booking-add/booking-add.component.ts
+++ src/app/booking-add/booking-add.component.ts
@@ -26,46 +26,28 @@ export class BookingAddComponent implements OnInit {
     x.object_name = Object;
     x.date_from = new Date(From);
     x.date_from.setDate( x.date_from.getDate() + 1 );
-
     x.date_to = new Date(To);
     x.date_to.setDate( x.date_to.getDate() + 1 );
-
     x.is_active = is_active;
-    x.amount = amount;
+    x.amount = Number(amount);
     x.currency = currency;
     x.comment = comment;
-
     this.bookings.push(x);
-
     // tslint:disable-next-line: prefer-for-of
     for (let index = 0; index < this.bookings.length; index++) {
       console.log(this.bookings[index]);
     }
-
     console.log(this.bookings.length);
   }
   storeindatabase() {
     // tslint:disable-next-line: prefer-for-of
     for (let index = 0; index < this.bookings.length; index++) {
-      this.bookingsFromBD[0].id = 0;
-      this.bookingsFromBD[0].object_name = this.bookings[index].object_name;
-      this.bookingsFromBD[0].amount = 2.40;
-      this.bookingsFromBD[0].currency = this.bookings[index].currency;
-      this.bookingsFromBD[0].comment = this.bookings[index].comment;
-      this.bookingsFromBD[0].is_active = this.bookings[index].is_active;
-      this.bookingsFromBD[0].date_from = this.bookings[index].date_from;
-      this.bookingsFromBD[0].date_to = this.bookings[index].date_to;
-      this.bookingsFromBD[0].capacity_provider_id = this.bookings[index].capacity_provider_id;
-      // tslint:disable-next-line: max-line-length
-      // this.bs.Post(this.bookings[index]);
-      this.bs.Post(this.bookingsFromBD[0]);
-      // this.ps.Post(this.providers[0]); // working
+      this.bs.Post(this.bookings[index]);
     }
     // tslint:disable-next-line: prefer-for-of
     for (let index = 0; index < this.bookings.length; index++) {
       this.bookings.pop();
     }
-
   }
 
   ngOnInit() {
