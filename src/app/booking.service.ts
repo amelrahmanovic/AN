cead9aa4e2fbcc04bc0f0f7981badaf01fa8a1d3
Amel <amelrahmanovic@hotmail.com>
2019-12-06 16:33:16 +0100

    Implementing HttpHeaders version from WEB API.

--- src/app/booking.service.ts
+++ src/app/booking.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
   uri = 'http://localhost:63510/api/Booking';
+  headers = new HttpHeaders();
+  v = '1.0';
 
   constructor(private http: HttpClient) { }
 
   Get() {
-    return this.http.get(`${this.uri}`);
+    return this.http.get(`${this.uri}`, {headers: {'api-version' : this.v}});
   }
   delete(id) {
     console.log(id);
-    return this.http.delete(`${this.uri}/${id}`);
+    return this.http.delete(`${this.uri}/${id}`, {headers: {'api-version' : this.v}});
   }
    Post(booking) {
     console.log(booking);
-    this.http.post(`${this.uri}`, booking).subscribe(res => console.log('Done'));
+    this.http.post(`${this.uri}`, booking, {headers: {'api-version' : this.v}}).subscribe(res => console.log('Done'));
   }
 }
--- src/app/provider.service.ts
+++ src/app/provider.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProviderService {
   uri = 'http://localhost:63510/api/Capacity';
+  headers = new HttpHeaders();
+  v = '1.0';
 
   constructor(private http: HttpClient) { }
 
   Get() {
-    return this.http.get(`${this.uri}`);
+    let headers = new HttpHeaders();
+    headers = headers.set('api-version', '1.0');
+
+    return this.http.get(`${this.uri}`, {headers: {'api-version' : this.v}});
   }
   Post(p) {
     console.log(p);
-    this.http.post(`${this.uri}`, p).subscribe(res => console.log('Added Done'));
+    this.http.post(`${this.uri}`, p, {headers: {'api-version' : this.v}}).subscribe(res => console.log('Added Done'));
   }
   Delete(id) {
     console.log('Deleted Done');
-    return this.http.delete(`${this.uri}/${id}`);
+    return this.http.delete(`${this.uri}/${id}`, {headers: {'api-version' : this.v}});
   }
 }
